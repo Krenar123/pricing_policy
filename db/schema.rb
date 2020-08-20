dbaf4d869a474b47800a6e4d66c25ea0c4b807c5
Krenar123 <krenardardhishta205@gmail.com>
2020-08-20 22:20:42 +0200

    Model location created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_201752) do
+ActiveRecord::Schema.define(version: 2020_08_20_202021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2020_08_20_201752) do
     t.index ["country_id"], name: "index_group_organizations_on_country_id"
   end
 
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.bigint "organization_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["organization_id"], name: "index_locations_on_organization_id"
+  end
+
   create_table "organizations", force: :cascade do |t|
     t.string "name"
     t.string "public_name"
@@ -45,4 +54,5 @@ ActiveRecord::Schema.define(version: 2020_08_20_201752) do
   end
 
   add_foreign_key "group_organizations", "countries"
+  add_foreign_key "locations", "organizations"
 end
