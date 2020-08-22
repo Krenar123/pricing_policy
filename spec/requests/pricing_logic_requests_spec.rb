a292b952ab69d75d8b62ec407fab772d1c74431d
Krenar123 <krenardardhishta205@gmail.com>
2020-08-22 13:25:09 +0200

    Testing request when posting base price

--- spec/requests/pricing_logic_requests_spec.rb
+++ spec/requests/pricing_logic_requests_spec.rb
@@ -1,10 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe "PricingLogicRequests" do
-  describe "GET /pricing_logic_requests" do
-    it "works! (now write some real specs)" do
-      get pricing_logic_requests_index_path
-      expect(response).to have_http_status(200)
-    end
+  describe 'Passing attributes with 3 different pricing policy' do
+    context 'when pricing policy is Flexible and base price 10' do
+      let(:group_organization) { create(:group_organization) }
+      
+      it 'should return the organizations of group_organizations with how many letters \'a\' appears in http://reuters.com divide by 100' do
+        result = HTTParty.get("http://reuters.com").count('a').to_i / 100
+        create(:organization, group_organization: group_organization)
+
+        post_params = {
+          params: {
+            model_type_name: 'Nope',
+            base_price: 10
+          }
+        }
+
+        post "/model_type_prices/#{group_organization.id}", post_params
+        
+        expect(response).to include(result)
+      end
+
+      it 'should return the organizations of group_organizations with how many words \'status\' appears in https://developer.github.com/v3/#http-redirects' do
+        result = HTTParty.get("https://developer.github.com/v3/#http-redirects").gsub(/[^a-z0-9\s]/i, ' ').split.select{ |word| word == 'status' || word == 'Status' }.count
+        create(:organization, pricing_policy: 'Fixed', group_organization: group_organization)
+
+        post_params = {
+          params: {
+            model_type_name: 'Nope',
+            base_price: 10
+          }
+        }
+
+        post "/model_type_prices/#{group_organization.id}", post_params
+
+        expect(response).to include(result)
+      end
+
+      it 'should return the organizations of group_organizations with how many letters \'a\' appears in http://reuters.com divide by 100' do
+        result = HTTParty.get("https://www.yourlocalguardian.co.uk/sport/rugby/rss/").parsed_response.to_s.gsub(/\W+/, ' ').split.select{ |word| word == 'pubDate' }.count
+        create(:organization, pricing_policy: 'Prestige', group_organization: group_organization)
+
+        post_params = {
+          params: {
+            model_type_name: 'Nope',
+            base_price: 10
+          }
+        }
+
+        post "/model_type_prices/#{group_organization.id}", post_params
+
+        expect(response).to include(result)
+      end
+    end 
   end
 end
