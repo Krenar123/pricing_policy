66c3f28e8e455be676b33bb7d62793ac6623c6b7
Krenar123 <krenardardhishta205@gmail.com>
2020-08-22 10:22:36 +0200

    Group controller refatored

--- app/controllers/group_organizations_controller.rb
+++ app/controllers/group_organizations_controller.rb
@@ -1,9 +1,7 @@
 class GroupOrganizationsController < ApplicationController
-    before_action :find_group
+    before_action :find_group_organizations
 
     def create_pricing
-        @organizations = @group_organization.organizations
-
         if @organizations.any?
             render json: PricingCreator.new(@organizations, price_params[:base_price]).call.to_json 
         else
@@ -17,7 +15,8 @@ class GroupOrganizationsController < ApplicationController
         params.permit(:group_organization_id, :model_type_name, :base_price)
     end
 
-    def find_group
+    def find_group_organizations
         @group_organization = GroupOrganization.find(params[:group_organization_id])
+        @organizations = @group_organization.organizations
     end
 end
